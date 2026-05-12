1a4b1b8b0856a6bbfe7e0e34eab3b9729497a106
Vidied <davidepan2001@gmail.com>
2026-05-12 17:22:07 +0200

    fix css pagina dettaglio

--- src/components/DettaglioArticolo.tsx
+++ src/components/DettaglioArticolo.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import type { SpaceFlightNews } from "../interfaces/Articolo";
 import { useEffect, useState } from "react";
 import { Spinner, Alert, Container } from "react-bootstrap";
+import DataArticolo from "./dataArticolo";
 
 const DettaglioArticolo = () => {
   const { id } = useParams<{ id: string }>();
@@ -27,24 +28,34 @@ const DettaglioArticolo = () => {
     fetchDettaglio();
   }, [id]);
 
-  if (loading)
-    return (
-      <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-    );
-  if (error || !articolo) return <Alert variant="danger">Errore!</Alert>;
+  const renderContent = () => {
+    if (loading)
+      return (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      );
+    if (error || !articolo) return <Alert variant="danger">Errore!</Alert>;
 
+    return (
+      <Container className="mt-5">
+        <h1>{articolo.title}</h1>
+        <img
+          src={articolo.image_url}
+          alt={articolo.title}
+          className="img-fluid my-4"
+        />
+        <p className="lead space-text2">{articolo.summary}</p>
+        <div className="d-flex justify-content-between">
+          <p>Fonte: {articolo.news_site}</p>Pubblicato il:{" "}
+          {DataArticolo(articolo.published_at)}
+        </div>
+      </Container>
+    );
+  };
   return (
-    <Container className="mt-5 min-vh-100">
-      <h1>{articolo.title}</h1>
-      <img
-        src={articolo.image_url}
-        alt={articolo.title}
-        className="img-fluid my-4"
-      />
-      <p className="lead space-text2">{articolo.summary}</p>
-      <p>Fonte: {articolo.news_site}</p>
+    <Container className="mt-0 min-vh-100 bg-space pt-5">
+      {renderContent()}
     </Container>
   );
 };
